e6b86fd26a90fddd97d1724d4b223285fe629ea8
Imran Ismail <m.imran@mymagic.my>
2015-08-13 02:38:44 +0800

    refactorization of the wrapper

---
+++ src/Client.php
@@ -0,0 +1,44 @@
+<?php 
+
+namespace MyMagic\Connect;
+use \GuzzleHttp\Client as BaseClient;
+
+class Client {
+  
+  private $client, $cipher, $compare_str, $email, $id;
+
+  public function __construct($cookie) {
+    $this->client = new BaseClient(array(
+      'base_uri'  =>  'http://connect.mymagic.my/api/',
+      'timeout'   =>  60,
+      'auth'      =>  array('magic', 'ilovemymagic'),
+      'debug'     =>  false
+    ));
+
+    $decoded_data = base64_decode($cookie);
+    $data = explode("|||", $decoded_data);
+
+    $this->id = $data[0];
+    $this->email = $data[1];
+    $this->cipher = $data[2];
+  }
+
+  public function getUserData() {
+    $json_string = $this->client->get('users/'.$this->id)->getBody()->getContents();
+    $data = json_decode($json_string)->data;
+    return $data;
+  }
+
+  public function isUserLoggedIn() {
+    return compareIvString();
+  }
+
+  private function compareIvString() {
+    $salt = "thisisaveryawesomemagicsalt12345";
+    $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
+    $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+    $iv_string = md5(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $salt, static::$email, MCRYPT_MODE_ECB, $iv));
+
+    return $this->cipher === $iv_string;
+  } 
+}
