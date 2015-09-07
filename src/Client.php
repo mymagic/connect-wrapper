c27eb7dffcb7be79455fb91e6e13814ca039585c
exiang <exiang83@yahoo.com>
2015-09-07 15:52:51 +0800

    update client

--- src/Client.php
+++ src/Client.php
@@ -14,6 +14,7 @@ class Client {
       'auth'      =>  array('magic', 'ilovemymagic'),
       'debug'     =>  false
     ));
+	$this->client->setDefaultOption('verify', false);
 
     $this->analyzeMagicCookie();
   }
@@ -30,35 +31,15 @@ class Client {
 	}
   }
   
-  public function hasMagicCookie() {
-	if(!empty($_COOKIE['magic_cookie'])) return true;
-	return false;
-  }
-  
   public function getUserData() {
+    // http://connect.mymagic.my/api/users/4
     $json_string = $this->client->get('users/'.$this->id)->getBody()->getContents();
     $data = json_decode($json_string)->data;
     return $data;
   }
-
+  
   public function isLoggedIn() {
     return $this->compareIvString();
-  }
-  
-  public function getEmail(){
-	return $this->email;
-  }
-  
-  public function getLoginUrl($redirectUrl=''){
-	return 'http://connect.mymagic.my/login?redirect_uri='.$redirectUrl;
-  }
-  
-  public function getLogoutUrl($redirectUrl=''){
-	return 'http://connect.mymagic.my/logout?redirect_uri='.$redirectUrl;
-  }
-  
-  public function getProfileUrl(){
-	return 'http://connect.mymagic.my/profile';
   }
 
   private function compareIvString() {
@@ -68,5 +49,110 @@ class Client {
     $iv_string = md5(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $salt, $this->email, MCRYPT_MODE_ECB, $iv));
 
     return $this->cipher === $iv_string;
-  } 
+  }
+	
+  
+	public function getLoginUrl($redirectUrl='')
+	{
+		return 'http://connect.mymagic.my/login?redirect_uri='.$redirectUrl;
+	}
+
+	public function getLogoutUrl($redirectUrl='')
+	{
+		return 'http://connect.mymagic.my/logout?redirect_uri='.$redirectUrl;
+	}
+
+	public function getProfileUrl()
+	{
+		return 'http://connect.mymagic.my/profile';
+	}
+
+	public function hasMagicCookie() 
+	{
+		if(!empty($_COOKIE['magic_cookie'])) return true;
+		return false;
+	}
+  
+	public function isUserExists($email) 
+	{
+		$email = urlencode($email);
+		// http://connect.mymagic.my/api/get-user-by-email?email=exiang83%40yahoo.com
+		$data = null;
+		
+		try 
+		{
+			$response = $this->client->get('get-user-by-email?email='.$email);
+			
+			if($response->getStatusCode() == 200)
+			{
+				$json_string = $response->getBody()->getContents();
+				$data = json_decode($json_string)->data;
+				if(!empty($data) && !empty($data->id)) return true;
+			}
+			else
+			{
+				return false;
+			}
+		} 
+		catch (\Exception $e) 
+		{
+			return false;
+		}
+		
+		return false;
+	}
+
+	public function getEmail()
+	{
+		return $this->email;
+	}
+  
+	public function createUser($email, $firstName='', $lastName='')
+	{
+		// http://connect.mymagic.my/api/signup
+		$email = urlencode($email);
+		$data = array('form_params'=>
+			array
+			(
+				'email'=>$email,
+				'first_name'=>str_replace('@', '', $firstName), 
+				'last_name'=>str_replace('@', '', $lastName)
+			)
+		);
+		
+		try
+		{
+			$res = $this->client->post('/signup');
+			echo $res->getStatusCode();
+		} 
+		catch (\Exception $e) 
+		{
+			echo $e->getMessage();
+		}
+		//print_r($res);echo $res->getStatusCode();exit;
+
+
+	}
+	
+	private function generateRandomPassword($max='8', $min='8', $lowerCase=false)
+	{
+		$limit = rand($min, $max);
+		$buffer = '';
+		for($i=0; $i<$limit; $i++)
+		{
+			$switch = rand(1, 4);
+			if($switch%2 == 0)
+			{
+				$buffer .= chr(rand(97, 122));
+			}
+			else
+			{
+				$buffer .= chr(rand(48,57));
+			}
+		}
+
+		if($lowerCase) $buffer = strtolower($buffer);
+		
+		return $buffer;
+	}
 }
