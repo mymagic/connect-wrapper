def60cf18037f88b9d6e8a555899bd4d2d07d6bc
hexenrobert <warezspirit@gmail.com>
2017-02-01 12:07:16 +0800

    MaGIC Connect Wrapper modification

--- README.md
+++ README.md
@@ -4,11 +4,31 @@
 composer install
 ```
 
-# Usage
+# 1.0 Usage
 
 ```
-Wrapper::init("http://theclienturl/); // Initialize the wrapper, throws exception if user is not logged in
-Wrapper::getUserData(); // {JSON} returns logged in user data
-Wrapper::isUserLoggedIn(); // {Boolean} returns true if user is logged in, false if otherwise
-Wrapper::$client_url; // {String} gets the client url
+
+$this->magicConnect = new MyMagic\Connect\Client; // Init MaGIC Connect
+$this->magicConnect->connect("CODE","CLIENTID","CLIENTSECRET","CALLBACKURI"); // Returns  {JSON} once the user authorize their account. Refer 1.1
+$this->magicConnect->getLogoutUrl("REDIRECTURI"); // Logout user from connect and return to logout user on client side
+$this->magicConnect->getProfileUrl(); // User connect profile URL
+$this->magicConnect->isUserExists("EMAIL") // Check whether user exist or not on connect
+$this->magicConnect->createUser("EMAIL","FIRSTNAME","LASTNAME","PASSWORD");
+
+```
+
+
+# 1.1 JSON Usage
+
+```
+
+{
+firstname: first_name // Foo
+lastname: last_name // Bar
+email: user_email // name@domain.tld
+gender: user_gender // M or F (Male/Female)
+country: user_country // Ex. MY (Malaysia)
+avatar: user_avatar // http://domain.tld/image.format
+}
+
 ```
--- src/Client.php
+++ src/Client.php
@@ -1,17 +1,29 @@
-<?php 
+<?php
 
 namespace MyMagic\Connect;
-use \GuzzleHttp\Client as BaseClient;
-use \GuzzleHttp\Exception\ClientException;
 
-class Client {
-  
-  private $client, $cipher, $compare_str, $email, $id;
-	
-  public function connect($request,$client_id,$client_secret,$uri){
+use \GuzzleHttp\Client as BaseClient;
+use \GuzzleHttp\Exception\ClientException as Exception;
+
+class Client
+{
+
+    private $url = "http://account.mymagic.my",
+        $token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6Ijg2ZmRmY2Y1NjhmZjMxMzhhMjA1NTA5MjFlZWRiZjdmMjJjN2VmZGZlYjUzNDA2YWMzODMyN2M5ZjUyMGI3NjlkN2QzMzFmZTcwYzA2ODk4In0.eyJhdWQiOiI4IiwianRpIjoiODZmZGZjZjU2OGZmMzEzOGEyMDU1MDkyMWVlZGJmN2YyMmM3ZWZkZmViNTM0MDZhYzM4MzI3YzlmNTIwYjc2OWQ3ZDMzMWZlNzBjMDY4OTgiLCJpYXQiOjE0ODU5MTk2MTAsIm5iZiI6MTQ4NTkxOTYxMCwiZXhwIjoxODAxNDUyNDEwLCJzdWIiOiIyMzIwIiwic2NvcGVzIjpbXX0.sNekvOu2iYKo4Sy3lJVq7DhSqidMm-e_s4HvyDdsrYvH4jodOnOkapz9hWd5tM2uMtK2EWZxprLKd5V9UexuNFX7qX_qHzQgNRZksBJLVC60uYxn-khLkwPSGyar_TPNUKBh3vujevnci0-rbZ0VIQq8BCUQALNdZCP6ltx6OQctAF1LxQAmT0T0hlpC6pTFr9qG0I0VJLnoyWZHIXEhXwKaWUKhPzLCiIn4GEqOVmnuYzPX4nl1lyenJvaRZHdr91XMIDGVKRKdb1HkwUpFtSn7lypExn4Uknans3V_3hj1O-GoKk4s7EWmokCJJjAzGGC29qZFkMAaJmH_IfWfFTT1fB7FJWt4Z873VCBIIU9Cioj9wPyVgtboWryDxs_vQfZFDI_I3ZwMbnomzTo44pOi8TVAnf25W7906pLMfUVhXBB_udDTaGwt5tC6oVLoiugyv6wEQh3RFw18Z1H01HaVsHmxdKSXaVRKyueBcMcHE9IY-s9OPY6K5LaTduQZsIXm2k36scm6WxQ2-pEygORX5wsibdezVk8BY67xk9ti7Z_Bnh5kSEh-2dxGTOfmFwBOvrHw4yPHe-IV5NgRbNQHmhopPZtZB5zqzMqk8I3pj3bnBT3C36TqcKSO_7z_3kHP8aU29yopzDwc0HyCvjQFetsUNBthinTgSYBtwhE";
+
+    public function redirect($url, $permanent = false) {
+        if($permanent) {
+            header('HTTP/1.1 301 Moved Permanently');
+        }
+        header('Location: '.$url);
+        exit();
+    }
+
+    public function connect($request, $client_id, $client_secret, $uri)
+    {
         $http = new BaseClient;
         try {
-            $response = $http->post('http://account.mymagic.my/oauth/token', [
+            $response = $http->post($this->url . '/oauth/token', [
                 'form_params' => [
                     'grant_type' => 'authorization_code',
                     'client_id' => $client_id,
@@ -20,18 +32,19 @@ class Client {
                     'code' => $request
                 ]
             ]);
-        } catch (\GuzzleHttp\Exception\ClientException $e) {
+        } catch (Exception $e) {
             $response = $e->getResponse();
             if ($response && $response->getStatusCode() === 401) {
-                return \Redirect::to('login')->with('alert-fail', 'These credentials do not match our records.');
+                return $this->redirect('/');
             }
         }
         try {
             $grab = json_decode((string)$response->getBody(), true)['access_token'];
         } catch (\Exception $e) {
-            return \Redirect::to('/error')->with('alert-fail', 'Permission Denied.');
+            return $this->redirect('/');
         }
-        $apiresponse = $http->get('http://account.mymagic.my/api/user', [
+
+        $apiresponse = $http->get($this->url . '/api/user', [
             'headers' => [
                 'Authorization' => 'Bearer ' . $grab,
                 'Content-Type' => 'application/json',
@@ -40,157 +53,59 @@ class Client {
         return json_decode((string)$apiresponse->getBody(), true);
     }
 
+    public function getLogoutUrl($redirectUrl = '')
+    {
+        return $this->url . 'logout?redirect_uri=' . $redirectUrl;
+    }
 
-  public function __construct() {
-    $this->client = new BaseClient(array(
-      'base_uri'  =>  'http://connect.mymagic.my/api/',
-      'timeout'   =>  60,
-      'auth'      =>  array('magic', 'ilovemymagic'),
-      'debug'     =>  false
-    ));
+    public function getProfileUrl()
+    {
+        return $this->url . '/profile';
+    }
 
-    $this->analyzeMagicCookie();
-  }
-    
-  public function analyzeMagicCookie() {
-	if($this->hasMagicCookie())
-	{
-		$decoded_data = base64_decode($_COOKIE['magic_cookie']);
-		$data = explode("|||", $decoded_data);
 
-		$this->id = $data[0];
-		$this->email = $data[1];
-		$this->cipher = $data[2];
-	}
-  }
-  
-  public function getUserData() {
-    // http://connect.mymagic.my/api/users/4
-    $json_string = $this->client->get('users/'.$this->id)->getBody()->getContents();
-    $data = json_decode($json_string)->data;
-    return $data;
-  }
-  
-  public function isLoggedIn() {
-    return $this->compareIvString();
-  }
+    public function isUserExists($email)
+    {
+        $http = new BaseClient;
+        $response = $http->get($this->url . '/api/get-user-details/' . $email, [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $this->token,
+                'Accept' => 'application/json',
+            ],
+        ]);
 
-  private function compareIvString() {
-    $salt = "thisisaveryawesomemagicsalt12345";
-    $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
-    $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
-    $iv_string = md5(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $salt, $this->email, MCRYPT_MODE_ECB, $iv));
+        if ($response && $response->getStatusCode() === 200) {
+            return true;
+        } else {
 
-    return $this->cipher === $iv_string;
-  }
-	
-  
-	public function getLoginUrl($redirectUrl='')
-	{
-		return 'http://connect.mymagic.my/login?redirect_uri='.$redirectUrl;
-	}
+            return false;
+        }
+    }
 
-	public function getLogoutUrl($redirectUrl='')
-	{
-		return 'http://connect.mymagic.my/logout?redirect_uri='.$redirectUrl;
-	}
+    public function createUser($email, $firstName = '', $lastName = '', $password, $gender = '', $t_and_c = '1', $confirmation = '1', $country = '')
+    {
+        try {
+            $http = new BaseClient;
+            $r = $http->post($this->url . 'register', array(
+                'form_params' => array(
+                    'email' => $email,
+                    'firstname' => str_replace('@', ' ', $firstName),
+                    'lastname' => str_replace('@', ' ', $lastName),
+                    'password' => $password,
+                    'password_confirmation' => $password,
+                    't_and_c' => $t_and_c,
+                    'confirmation' => $confirmation,
+                    'gender' => $gender,
+                    'country' => $country
+                )
+            ));
 
-	public function getProfileUrl()
-	{
-		return 'http://connect.mymagic.my/profile';
-	}
+            if ($r->getStatusCode() == 200) {
+                return true;
+            }
+        } catch (Exception $e) {
+            return false;
+        }
+    }
 
-	public function hasMagicCookie() 
-	{
-		if(!empty($_COOKIE['magic_cookie'])) return true;
-		return false;
-	}
-  
-	public function isUserExists($email) 
-	{
-		$email = urlencode($email);
-		// http://connect.mymagic.my/api/get-user-by-email?email=exiang83%40yahoo.com
-		$data = null;
-		
-		try 
-		{
-			$response = $this->client->get('get-user-by-email?email='.$email);
-			
-			if($response->getStatusCode() == 200)
-			{
-				$json_string = $response->getBody()->getContents();
-				$data = json_decode($json_string)->data;
-				if(!empty($data) && !empty($data->id)) return true;
-			}
-			else
-			{
-				return false;
-			}
-		} 
-		catch (\Exception $e) 
-		{
-			return false;
-		}
-		
-		return false;
-	}
-
-	public function getEmail()
-	{
-		return $this->email;
-	}
-  
-	public function createUser($email, $firstName='', $lastName='')
-	{
-		// some how email should not be urlencode else it failed
-		//$email = urlencode($email);
-		
-		try
-		{
-			// http://connect.mymagic.my/api/signup
-			$r = $this->client->post('signup', array(
-				'form_params' => array(
-					'email' => $email, 
-					'first_name' => str_replace('@', ' ', $firstName),
-					'last_name' => str_replace('@', ' ', $lastName),
-				)
-			));
-			
-			if($r->getStatusCode() == 200)
-			{
-				return true;
-			}
-		}
-		catch (\Exception $e) 
-		{
-			/*echo "<pre>";
-			echo "Status code: " . $e->getResponse()->getStatusCode();
-			echo "<br>";
-			print_r( $e->getResponse()->getBody()->getContents());*/
-			return false;
-		}
-
-	}
-	
-	private function generateRandomPassword($max='8', $min='8', $lowerCase=false)
-	{
-		$limit = rand($min, $max);
-		$buffer = '';
-		for($i=0; $i<$limit; $i++)
-		{
-			$switch = rand(1, 4);
-			if($switch%2 == 0)
-			{
-				$buffer .= chr(rand(97, 122));
-			}
-			else
-			{
-				$buffer .= chr(rand(48,57));
-			}
-		}
-
-		if($lowerCase) $buffer = strtolower($buffer);
-		
-		return $buffer;
-	}
 }
